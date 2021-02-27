188589469b3191d9867b109ba5bac72090a59757
Kaustav Ghosh <teetangh@gmail.com>
2021-02-27 21:16:08 +0530

    Added Procfile

--- TikTok/tiktok-backend/server.js
+++ TikTok/tiktok-backend/server.js
@@ -6,14 +6,14 @@ import Videos from "./dbModel.js";
 
 // app config
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 // middlewares
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeaders("Access-Control-Allow-Origin", "*"),
-    res.setHeaders("Access-Control-Allow-Headers", "*"),
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
     next();
 });
 
